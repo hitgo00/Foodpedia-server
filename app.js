130381ecd1344f66ebc4cd71183613a4a6889860
Het Paghadal <201801262@daiict.ac.in>
2021-04-06 15:59:20 +0530

    Added Update Item and Get Item API

--- app.js
+++ app.js
@@ -16,65 +16,103 @@ dotenv.config();
 
 // Connect to DB Atlas
 mongoose
-  .connect(process.env.DB_url, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-  })
-  .then(() => {
-    console.log('Connected to DB');
-  })
-  .catch((e) => {
-    console.log('Database Error');
-    console.log(e);
-  });
+    .connect(process.env.DB_url, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true
+    })
+    .then(() => {
+        console.log('Connected to DB');
+    })
+    .catch((e) => {
+        console.log('Database Error');
+        console.log(e);
+    });
 
 // Models / Schemas
 const UserSchema = new mongoose.Schema({
-  username: String,
-  email: String,
-  food_journal_items: [
-    {
-      food_name: String,
-      image_url: String,
-      created: String,
-      metadata: String,
-    },
-  ],
+    username: String,
+    email: String,
+    food_journal_items: [{
+        food_name: String,
+        image_url: String,
+        created: String,
+        metadata: String,
+    },],
 });
 const User = mongoose.model('User', UserSchema);
 
 // Routes ..
 app.get('/', (req, res) => {
-  res.send('<h1>FoodPedia</h1>');
+    res.send('<h1>FoodPedia</h1>');
+});
+
+// Send All food_journal_items for given email
+app.get('/getfooditems',async (req, res) => {
+    try {
+        console.log(req.query.email);
+        const items =await User.find({ email: req.query.email },, { food_journal_items: 1 });
+        
+        console.log(items);
+        const response = {
+            ok: true,
+            data: items,
+            msg: "Data Send Successfully"
+        }
+        res.status(200).send(response);
+    }
+    catch (err) {
+        console.log(err)
+        res.status(400).ssend(err);
+    }
+});
+
+// Add items to journal => Request should have email and food_journal_item Object.. 
+app.post('/additem',async (req, res) => {
+    try {
+        console.log(req.body);
+        const items =await User.updateOne({ email: req.query.email },
+            { $push: { food_journal_items: req.body.food_journal_items } });
+        const response = {
+            ok: true,
+            msg: "Item added Successfully"
+        }
+        res.status(200).send(response);
+    }
+    catch (err) {
+        console.log(err)
+        res.status(400).send(err);
+    }
 });
 
 // For testing
-app.post('/additem', (req, res) => {
-  // Add to the database
-  const date = new Date().toJSON().slice(0, 10).replace(/-/g, '/');
-  console.log(req.body);
-  const user = new User({
-    username: 'hemang_test', // will be retrieved from the gid
-    email: req.query.email,
-    food_journal_items: [
-      {
-        food_name: req.body.food_name,
-        image_url: req.body.img_url,
-        created: date,
-        metadata: req.body.description,
-      },
-    ],
-  });
+app.post('/adduser',async (req, res) => {
+    // Add to the database
+    const date = new Date().toJSON().slice(0, 10).replace(/-/g, '/');
+    console.log(req.body);
 
-  user.save((err, result) => {
-    if (err) res.send(err);
-    else {
-      console.log('FoodItem Added');
-      res.send(result);
-    }
-  });
+    const chk=await User.find({email : req.query.email});
+    if(chk) {res.send("user already exists");}
+    const user = new User({
+        username: 'hemang_test', // will be retrieved from the gid
+        email: req.query.email,
+        food_journal_items: [{
+            food_name: req.body.food_journal_items.food_name,
+            image_url: req.body.food_journal_items.image_url,
+            created: date,
+            metadata: req.body.food_journal_items.metadata,
+        },],
+    });
+    await user.save((err, result) => {
+        if (err) res.send(err);
+        else {
+            console.log('FoodItem Added');
+            res.send(result);
+        }
+    });
 });
+
+
 
 // Server Listening
 const PORT = process.env.PORT || 3000;
